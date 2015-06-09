253c5da5d71c2feb4708d546b94a04b525a3b567
dr0ptab1e <rfp.overdriver@gmail.com>
2015-06-09 19:31:48 +0400

    теперь без асинктасок

--- app/src/main/java/firstproject/populator/MainActivity.java
+++ app/src/main/java/firstproject/populator/MainActivity.java
@@ -39,83 +39,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pd=new ProgressDialog(MainActivity.this);
+        pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pd.setCancelable(false);
         View.OnClickListener oclBtnCreate=new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final AsyncTask task=new AsyncTask() {
-                    @Override
-                    protected Object doInBackground(Object[] params) {
-                        for(int i=0;i<Integer.parseInt(((EditText) findViewById(R.id.textAmount)).getText().toString());i++){
-                            if(Thread.interrupted()){
-                                return -1;
-                            }
-                            Random r=new Random();
-                            StringBuilder newNumber=new StringBuilder("+7");
-                            for(int l=0;l<10;l++) {
-                                newNumber.append(r.nextInt(10));
-                            }
-                            String newName="";
-                            int fileSize=0;
-                            try{
-                                InputStream is=getResources().openRawResource(R.raw.names);
-                                BufferedReader reader=new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.names)));
-                                try {
-                                    byte[] c = new byte[1024];
-                                    int readChars = 0;
-                                    while ((readChars = is.read(c)) != -1) {
-                                        for (int p = 0; p < readChars; ++p) {
-                                            if (c[p] == (byte)'\n') {
-                                                ++fileSize;
-                                            }
-                                        }
-                                    }
-                                    int qwe=r.nextInt(fileSize);
-                                    for(int line=0;line<qwe;line++){
-                                        reader.readLine();
-                                    }
-                                    newName=reader.readLine();
-                                } finally {
-                                    is.close();
-                                    reader.close();
-                                }
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            addContact(newName + "_" + ((EditText)findViewById(R.id.suffix)).getText().toString(),newNumber.toString());
-                            publishProgress(i+1);
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onPostExecute(Object o) {
-                        super.onPostExecute(o);
-                        Toast.makeText(getApplicationContext(), R.string.done, Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    protected void onProgressUpdate(Object[] values) {
-                        super.onProgressUpdate(values);
-                        pd.setProgress((Integer)values[0]);
-                        if(pd.getProgress()==pd.getMax()){
-                            pd.dismiss();
-                        }
-                    }
-                };
-                task.execute();
                 int amount=Integer.parseInt(((EditText) findViewById(R.id.textAmount)).getText().toString());
+                String suffix=((EditText)findViewById(R.id.suffix)).getText().toString();
+                MyIntentService.startActionCreateContacts(getApplicationContext(), amount, suffix, new ProgressReceiver(new Handler()));
                 if(amount>10){
-                    pd=new ProgressDialog(MainActivity.this);
                     message=R.string.pdCreating;
                     pd.setTitle(message);
-                    pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                    pd.setProgress(0);
                     pd.setMax(amount);
-                    pd.setOwnerActivity(MainActivity.this);
                     pd.setButton(Dialog.BUTTON_POSITIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            task.cancel(true);
+                            stopService(new Intent(MainActivity.this, MyIntentService.class));
                         }
                     });
                     pd.show();
@@ -127,57 +68,20 @@ public class MainActivity extends Activity {
         View.OnClickListener oclBtnRemove=new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final AsyncTask task=new AsyncTask() {
-                    @Override
-                    protected Object doInBackground(Object[] params) {
-                        int progress=0;
-                        Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
-                        Cursor cur = getApplicationContext().getContentResolver().query(contactUri, null, null, null, null);
-                        try {
-                            if (cur.moveToFirst()) {
-                                do {
-                                    if(Thread.interrupted()){
-                                        return -1;
-                                    }
-                                    if (cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)).endsWith("_"+((EditText) findViewById(R.id.suffix)).getText().toString())) {
-                                        String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
-                                        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
-                                        progress+=getApplicationContext().getContentResolver().delete(uri, null, null);
-                                        pd.setProgress(progress);
-                                        if(pd.getProgress()==pd.getMax()){
-                                            pd.dismiss();
-                                        }
-                                    }
-                                } while (cur.moveToNext());
-                            }
-                        } catch (Exception e) {
-                            System.out.println(e.getStackTrace());
-                        } finally {
-                            cur.close();
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onPostExecute(Object o) {
-                        super.onPostExecute(o);
-                        Toast.makeText(getApplicationContext(), R.string.done, Toast.LENGTH_LONG).show();
-                    }
-                };
-                task.execute();
-                int amount=countContacts(getApplicationContext(), ((EditText) findViewById(R.id.suffix)).getText().toString());
+                String suffix=((EditText) findViewById(R.id.suffix)).getText().toString();
+                MyIntentService.startActionRemoveContacts(getApplicationContext(), suffix, new ProgressReceiver(new Handler()));
+                int amount=countContacts(getApplicationContext(), suffix);
                 if(amount>10){
-                    pd=new ProgressDialog(MainActivity.this);
                     message=R.string.pdRemoving;
                     pd.setTitle(message);
-                    pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                    pd.setProgress(0);
                     pd.setMax(amount);
                     pd.setButton(Dialog.BUTTON_POSITIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            task.cancel(true);
+                            stopService(new Intent(MainActivity.this, MyIntentService.class));
                         }
                     });
-                    pd.setOwnerActivity(MainActivity.this);
                     pd.show();
                 }
             }
@@ -198,17 +102,13 @@ public class MainActivity extends Activity {
             }
         });
         findViewById(R.id.btnRemove).setEnabled(false);
-        if(pd!=null){
-            pd.show();
-        }
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
-        //Log.e("GOVNO", new Integer(pd.getProgress()).toString());
         if(pd!=null && pd.isShowing()){
             ProgressReceiver receiver=new ProgressReceiver(new Handler());
-            MyIntentService.startActionRedrawPD(getApplicationContext(),pd.getProgress(),message,receiver);
+            MyIntentService.startActionRedrawPD(getApplicationContext(), pd.getProgress(), message, receiver);
         }
         super.onConfigurationChanged(newConfig);
     }
@@ -235,7 +135,7 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static int countContacts(Context ctx, String suffix) {
+    private static int countContacts(Context ctx, String suffix) {
         Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
         Cursor cur = ctx.getContentResolver().query(contactUri, null, null, null, null);
         int count=0;
@@ -257,53 +157,18 @@ public class MainActivity extends Activity {
         return count;
     }
 
-    private void addContact(String name, String mobileNumber){
-        ArrayList<ContentProviderOperation> ops = new ArrayList <ContentProviderOperation> ();
-
-        ops.add(ContentProviderOperation.newInsert(
-                ContactsContract.RawContacts.CONTENT_URI)
-                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
-                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
-                .build());
-
-        //------------------------------------------------------ Names
-        ops.add(ContentProviderOperation.newInsert(
-                ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE,
-                        ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-                .withValue(
-                        ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
-                .build());
-        //------------------------------------------------------ Mobile Number
-
-        ops.add(ContentProviderOperation.
-                newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE,
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber)
-                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
-                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
-                .build());
-        // Asking the Contact provider to create a new contact
-        try {
-            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
     private class ProgressReceiver extends ResultReceiver{
 
         @Override
         protected void onReceiveResult(int resultCode, Bundle resultData) {
             super.onReceiveResult(resultCode, resultData);
             int progress=resultData.getInt("progress");
-            int message=resultData.getInt("message");
             pd.setProgress(progress);
-            pd.setMessage(getString(message));
-            pd.show();
+            if(pd.getMax()==progress){
+                pd.dismiss();
+                Toast.makeText(getApplicationContext(),R.string.done,Toast.LENGTH_SHORT).show();
+                return;
+            }
         }
 
         public ProgressReceiver(Handler handler) {
--- app/src/main/java/firstproject/populator/MyIntentService.java
+++ app/src/main/java/firstproject/populator/MyIntentService.java
@@ -1,13 +1,34 @@
 package firstproject.populator;
 
 import android.app.IntentService;
+import android.content.ContentProviderOperation;
 import android.content.Intent;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.provider.ContactsContract;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MyIntentService extends IntentService {
     private static final String ACTION_REDRAW_PD = "firstproject.populator.action.REDRAW_PD";
+    private static final String ACTION_CREATE_CONTACTS = "firstproject.populator.action.CREATE_CONTACTS";
+    private static final String ACTION_REMOVE_CONTACTS = "firstproject.populator.action.REMOVE_CONTACTS";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    boolean interruptNow=false;
 
     public static void startActionRedrawPD(Context context, int progress, int message, ResultReceiver receiver) {
         Intent intent = new Intent(context, MyIntentService.class);
@@ -16,8 +37,18 @@ public class MyIntentService extends IntentService {
         context.startService(intent);
     }
 
-    public MyIntentService() {
-        super("MyIntentService");
+    public static void startActionCreateContacts(Context context, int amount, String suffix, ResultReceiver receiver){
+        Intent intent=new Intent(context,MyIntentService.class);
+        intent.setAction(ACTION_CREATE_CONTACTS);
+        intent.putExtra("receiver",receiver).putExtra("amount", amount).putExtra("suffix", suffix);
+        context.startService(intent);
+    }
+
+    public static void startActionRemoveContacts(Context context, String suffix, ResultReceiver receiver){
+        Intent intent=new Intent(context,MyIntentService.class);
+        intent.setAction(ACTION_REMOVE_CONTACTS);
+        intent.putExtra("receiver",receiver).putExtra("suffix",suffix);
+        context.startService(intent);
     }
 
     @Override
@@ -25,14 +56,136 @@ public class MyIntentService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_REDRAW_PD.equals(action)) {
-                final int progress = intent.getIntExtra("progress",0);
-                final int message = intent.getIntExtra("message",R.string.pdCreating);
+                final int progress = intent.getIntExtra("progress", 0);
+                final int message = intent.getIntExtra("message", R.string.pdCreating);
                 final ResultReceiver receiver=intent.getParcelableExtra("receiver");
                 Bundle data=new Bundle();
                 data.putInt("progress",progress);
                 data.putInt("message",message);
                 receiver.send(0,data);
             }
+            if (ACTION_CREATE_CONTACTS.equals(action)){
+                final int amount = intent.getIntExtra("amount",0);
+                final String suffix = intent.getStringExtra("suffix");
+                final ResultReceiver receiver=intent.getParcelableExtra("receiver");
+                try{
+                    InputStream is = getResources().openRawResource(R.raw.names);
+                    try {
+                        int fileSize = 0;
+                        byte[] c = new byte[1024];
+                        int readChars = 0;
+                        while ((readChars = is.read(c)) != -1) {
+                            for (int p = 0; p < readChars; ++p) {
+                                if (c[p] == (byte) '\n') {
+                                    ++fileSize;
+                                }
+                            }
+                        }
+                        for (int i = 0; i < amount; i++) {
+                            if (interruptNow) {
+                                return;
+                            }
+                            Random r = new Random();
+                            StringBuilder newNumber = new StringBuilder("+7");
+                            for (int l = 0; l < 10; l++) {
+                                newNumber.append(r.nextInt(10));
+                            }
+                            String newName = "";
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.names)));
+                            try {
+                                int qwe = r.nextInt(fileSize);
+                                for (int line = 0; line < qwe; line++) {
+                                    reader.readLine();
+                                }
+                                newName = reader.readLine();
+                            } finally {
+                                reader.close();
+                            }
+                            addContact(newName + "_" + suffix, newNumber.toString());
+                            Bundle data=new Bundle();
+                            data.putInt("progress", i + 1);
+                            receiver.send(1,data);
+                        }
+                    }
+                    finally{
+                        is.close();
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(ACTION_REMOVE_CONTACTS.equals(action)){
+                final String suffix=intent.getStringExtra("suffix");
+                final ResultReceiver receiver=intent.getParcelableExtra("receiver");
+                int progress=0;
+                Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
+                Cursor cur = getApplicationContext().getContentResolver().query(contactUri, null, null, null, null);
+                try {
+                    if (cur.moveToFirst()) {
+                        do {
+                            if(interruptNow){
+                                return;
+                            }
+                            if (cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)).endsWith("_"+suffix)) {
+                                String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+                                Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                                progress+=getApplicationContext().getContentResolver().delete(uri, null, null);
+                                Bundle data=new Bundle();
+                                data.putInt("progress",progress);
+                                receiver.send(1,data);
+                            }
+                        } while (cur.moveToNext());
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getStackTrace());
+                } finally {
+                    cur.close();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        interruptNow=true;
+    }
+
+    private void addContact(String name, String mobileNumber){
+        ArrayList<ContentProviderOperation> ops = new ArrayList <ContentProviderOperation> ();
+
+        ops.add(ContentProviderOperation.newInsert(
+                ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+
+        //------------------------------------------------------ Names
+        ops.add(ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE,
+                        ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(
+                        ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
+                .build());
+        //------------------------------------------------------ Mobile Number
+
+        ops.add(ContentProviderOperation.
+                newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                .build());
+        // Asking the Contact provider to create a new contact
+        try {
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 }
